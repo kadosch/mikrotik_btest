02a0e7df411b5e7775fe2795709996a30afb7f47
kadosch <kadosch86@gmail.com>
2012-12-15 22:47:46 +0100

    Test más robusto

--- tcptest_thread.c
+++ tcptest_thread.c
@@ -41,21 +41,20 @@ void init_thread_args(thread_args_t *args, pthread_mutex_t *mutex, uint16_t bufs
 
 
 void *tcptest_thread(void *argument){
-	int32_t sockfd, rv;
+	int32_t sockfd, poll_ctrl;
 	thread_args_t *args;
 	uint8_t *buffer;
+	uint16_t failed = 0;
 	int32_t bytes = 0;
 	struct pollfd pfd;
 
 	args = (thread_args_t *) argument;
 
 	if ((sockfd = open_socket(args->host, args->port)) == RETURN_ERROR)
-			return NULL;
+			goto exit;
 
-	if (init_test(sockfd, args->user, args->password, args->direction, args->bufsize) == RETURN_ERROR){
-			close(sockfd);
-			return NULL;
-	}
+	if (init_test(sockfd, args->user, args->password, args->direction, args->bufsize) == RETURN_ERROR)
+			goto close_and_exit;
 
 	fcntl(sockfd, F_SETFL, O_NONBLOCK);
 	pfd.fd = sockfd;
@@ -70,21 +69,44 @@ void *tcptest_thread(void *argument){
 	pthread_mutex_lock(args->mutex);
 	while (args->alive){
 		pthread_mutex_unlock(args->mutex);
-		rv = poll(&pfd, 1, POLL_TIMEOUT);
-		if (rv == -1 || rv == 0)
-			break;
-		if ((pfd.revents & POLLIN) && (args->direction == RECEIVE))
-			bytes = recv(sockfd, buffer, args->bufsize, 0);
-		else if ((pfd.revents & POLLOUT) && (args->direction == SEND))
-			bytes = send(sockfd, buffer, args->bufsize, MSG_NOSIGNAL);
-		else
-			break;
-		if (bytes == -1)
-			break;
+		poll_ctrl = poll(&pfd, 1, POLL_TIMEOUT);
+		switch (poll_ctrl){
+			case (-1 | 0):
+				++failed;
+				bytes = 0;
+				break;
+			default:
+				switch (pfd.revents) {
+					case POLLIN:
+						if (args->direction == RECEIVE)
+							bytes = recv(sockfd, buffer, args->bufsize, 0);
+						break;
+					case POLLOUT:
+						if (args->direction == SEND)
+							bytes = send(sockfd, buffer, args->bufsize, MSG_NOSIGNAL);
+						break;
+					default:
+						++failed;
+						bytes = 0;
+						break;
+					}
+					if (bytes == -1)
+						++failed;
+					else
+						failed = 0;
+					if(failed >= MAX_RETRY){
+						goto free_and_exit;
+				}
+				break;
+		}
 		pthread_mutex_lock(args->mutex);
 		args->bytes += bytes;
 	}
+free_and_exit:
 	free(buffer);
+close_and_exit:
+	close(sockfd);
+exit:
 	pthread_mutex_trylock(args->mutex);
 	args->alive = 0;
 	pthread_mutex_unlock(args->mutex);
